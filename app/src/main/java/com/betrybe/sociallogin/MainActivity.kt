d89873e1fca1a41e01222a57c0f34d7cf98505d3
junior8319 <tonhaun@yahoo.com.br>
2025-07-15 16:38:47 -0300

    feat-req 8-validates email.

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.betrybe.sociallogin
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
@@ -15,27 +17,35 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.input_password_text)
     }
     private val btnLogin: MaterialButton by lazy { findViewById(R.id.login_button) }
+    private val emailWarningText: TextView by lazy { findViewById(R.id.text_invalid_email) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailWarningText.visibility = View.GONE
 
         btnLogin.setOnClickListener {
             val typedEmail = inputEmail.text.toString().trim()
 
             if (typedEmail.isEmpty()) {
                 inputEmail.error = "Email é obrigatório"
+                emailWarningText.visibility = View.VISIBLE
             } else if (!validateEmail(typedEmail)) {
-                inputEmail.error = "Email inválido"
+                emailWarningText.visibility = View.VISIBLE
             } else {
                 inputEmail.error = null
+                emailWarningText.visibility = View.GONE
             }
         }
 
         val inputsWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
-            override fun afterTextChanged(s: Editable?) = checkFields()
+            override fun afterTextChanged(s: Editable?) {
+                checkFields()
+                emailWarningText.visibility = View.GONE
+            }
         }
 
         inputEmail.addTextChangedListener(inputsWatcher)
@@ -44,11 +54,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun validateEmail(typedEmail: String): Boolean {
         val emailPattern = Regex(
-        "^[A-Za-z0-9.]+" +
-               "@" +
-               "[A-Za-z]+" +
-               "\\." +
-               "[A-Za-z]+$"
+        "^[A-Za-z0-9.]{2,}@[A-Za-z]{2,}(?:\\.[A-Za-z]*)+$"
         )
 
         return emailPattern.matches(typedEmail)
